0436c9c151716f18bcbe16fae7623c6abb4df91c
harshithatiruveedula <harshithatiruveedula@gmail.com>
2025-12-21 22:19:56 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-import views   # import views from current folder
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('services/', views.services, name='services'),
-    path('contact/', views.contact, name='contact'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('api/chat/', views.chat_api, name='chat_api'),
+    path('', include('main.urls')),
 ]
+
