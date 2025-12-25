5206fc3d6ed6ed4d7a51b4cc5629eca0909bae33
harshithatiruveedula <harshithatiruveedula@gmail.com>
2025-12-25 13:42:30 +0530

    Add create-admin path to urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
+    path('create-admin/', views.create_admin),
 ]
+
 
 
